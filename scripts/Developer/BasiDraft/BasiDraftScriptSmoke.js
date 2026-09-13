5e2e5cd4b182f532c4417b0e9f611e1d1bed0678
an04wallt-spec <an04wallt@gmail.com>
2026-09-13 17:59:45 +0300

    Verify first automatic dimensions numerically in QCAD runtime

--- scripts/Developer/BasiDraft/BasiDraftScriptSmoke.js
+++ scripts/Developer/BasiDraft/BasiDraftScriptSmoke.js
@@ -4,4 +4,22 @@ if (typeof(BasiDraftOverall) === "undefined") {
     throw new Error("BasiDraftOverall action was not loaded");
 }
 
-print("BasiDraft script smoke test passed");
+var testBox = new RBox(20.0, 30.0, 120.0, 80.0);
+var data = BasiDraftOverall.createDimensionData(testBox, 10.0);
+
+if (data.length !== 2) {
+    throw new Error("Expected two overall dimensions, got " + data.length);
+}
+
+var width = data[0].getMeasuredValue();
+var height = data[1].getMeasuredValue();
+
+if (Math.abs(width - 100.0) > 1.0e-9) {
+    throw new Error("Wrong width dimension: " + width);
+}
+
+if (Math.abs(height - 50.0) > 1.0e-9) {
+    throw new Error("Wrong height dimension: " + height);
+}
+
+print("BasiDraft script smoke test passed: 100 x 50");
