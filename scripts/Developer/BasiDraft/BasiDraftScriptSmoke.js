f7f5916c74f1d1f466ca747c0c7dfd135e15704e
an04wallt-spec <an04wallt@gmail.com>
2026-09-13 17:56:41 +0300

    Add headless smoke test for BasiDraft scripts

---
+++ scripts/Developer/BasiDraft/BasiDraftScriptSmoke.js
@@ -0,0 +1,7 @@
+include("scripts/Draw/Dimension/BasiDraftOverall/BasiDraftOverall.js");
+
+if (typeof(BasiDraftOverall) === "undefined") {
+    throw new Error("BasiDraftOverall action was not loaded");
+}
+
+print("BasiDraft script smoke test passed");
